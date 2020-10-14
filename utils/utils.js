4577e42dc15511341bc797eccf95726d29cc5d01
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-13 23:17:14 -0700

    added generateSelection and findById functions to utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,24 @@
 import { pokeData } from '../data/pokemon.js';
+
+export function generateSelection(){
+    const threeOptions = [];
+    let option1, option2, option3 = 0;
+    do {
+        option1 = Math.floor(Math.random() * pokeData.length);
+        option2 = Math.floor(Math.random() * pokeData.length);
+        option3 = Math.floor(Math.randon() * pokeData.length);
+    } while (option1 === option2 || option2 === option3 || option1 === option3);
+    let randomOption1 = pokeData[option1];
+    let randomOption2 = pokeData[option2];
+    let randomOption3 = pokeData[option3];
+    threeOptions.push(randomOption1, randomOption2, randomOption3);
+    return threeOptions;
+}
+
+export function findById(id, array) {
+    const item = null;
+    for(let i = 0; i < array.length; i++) {
+        if (id === array [i].id) return array[i];
+    }
+    return item;
+}
