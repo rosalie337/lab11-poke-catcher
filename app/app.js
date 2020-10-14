f9a6dc7addfb3446f65b81aed5698fde40effca2
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-13 23:20:35 -0700

    imported functions from utils.js in app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 // import functions 
-import { pokeData } from '../data/pokemon.js';
-
+//import { pokeData } from '../data/pokemon.js';
+import { generateSelection, findById } from '../utils/utils.js';
 //grab DOM elements
 const resetButton = document.getElementById('reset-button');
 const clearButton = document.getElementById('clear-button');
@@ -10,7 +10,7 @@ const images = document.querySelectorAll('poke-image');
 
 let count = 0;
 let encountered = [];
-const remainingPokemon = pokeData.slice();
+//const remainingPokemon = pokeData.slice();
 
 
 // set event listeners to update state and DOM
