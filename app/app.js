e04f8ed446d938bdc4d5f2e760d38e3d3fc31c9d
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-15 08:06:52 -0700

    added name but not rendering

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 //import functions 
 import { pokeData } from '../data/pokemon.js';
 import { findById } from '../utils/utils.js';
+
 //grab DOM elements
-const name = document.querySelector('h2');
+const name = document.querySelectorAll('h2'); // not rendering
 const images = document.querySelectorAll('img');
 const radioButtons = document.querySelectorAll('input');
 
@@ -24,7 +25,7 @@ function generateGamePlay() {
     
     generateSelection()
     for(let i= 0; radioButtons < i; i++) {
-        
+
     }
         radio[i].addEventListener()
     for(let i = 0; i < selection.length; i++){
@@ -41,25 +42,25 @@ function generateSelection(){
     let option1 = Math.floor(Math.random() * pokeData.length);
     let option2 = 0
     let option3 = 0
-    console.log(option1);
-         do {
-
+        do {
             option2 = Math.floor(Math.random() * pokeData.length);
             option3 = Math.floor(Math.random() * pokeData.length);
    
-    } while (option1 === option2 || option2 === option3 || option1 === option3);
-    let randomOption1 = pokeData[option1];
-    let randomOption2 = pokeData[option2];
-    let randomOption3 = pokeData[option3];
+        } while (option1 === option2 || option2 === option3 || option1 === option3);
+            let randomOption1 = pokeData[option1];
+            let randomOption2 = pokeData[option2];
+            let randomOption3 = pokeData[option3];
     
-    console.log(randomOption1);
-
+    
+    name[0].value = randomOption1.pokemon; // not rendering
     radioButtons[0].value = randomOption1.id;
     images[0].src = randomOption1.url_image;
 
+    name[1].value = randomOption2.pokemon; // not rendering
     radioButtons[1].value = randomOption2.id;
     images[1].src =randomOption2.url_image;
 
+    name[2].value = randomOption1.pokemon; // not rendering
     radioButtons[2].value = randomOption3.id;
     images[2].src =randomOption3.url_image;
 }
