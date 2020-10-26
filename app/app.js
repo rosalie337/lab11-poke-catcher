90cfa97518873f89ca17bf7a9425a499f74e2d85
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-25 20:13:14 -0700

    added results.html and added event listener to app.js

--- app/app.js
+++ app/app.js
@@ -3,12 +3,13 @@ import { pokeData } from '../data/pokemon.js';
 import { findById } from '../utils/utils.js';
 
 //grab DOM elements
-const name = document.querySelectorAll('h2'); // not rendering
+// const name = document.querySelectorAll('h2'); // not rendering
 const images = document.querySelectorAll('img');
 const radioButtons = document.querySelectorAll('input');
+const userCaptures = document.querySelector('user-captures');
+const moreButton = document.querySelector('more-button');
 
 // initialize state
-
 let count = 0;
 let encountered = [];
 
@@ -52,17 +53,20 @@ function generateSelection(){
             let randomOption3 = pokeData[option3];
     
     
-    name[0].value = randomOption1.pokemon; // not rendering
+    // name[0].value = randomOption1.pokemon; // not rendering
     radioButtons[0].value = randomOption1.id;
     images[0].src = randomOption1.url_image;
+    radioButtons[0].check = false;
 
-    name[1].value = randomOption2.pokemon; // not rendering
+    // name[1].value = randomOption2.pokemon; // not rendering
     radioButtons[1].value = randomOption2.id;
     images[1].src =randomOption2.url_image;
+    radioButtons[1].check = false;
 
-    name[2].value = randomOption1.pokemon; // not rendering
+    // name[2].value = randomOption1.pokemon; // not rendering
     radioButtons[2].value = randomOption3.id;
     images[2].src =randomOption3.url_image;
+    radioButtons[2].check = false;
 }
 
 function catches(id) {
@@ -92,4 +96,15 @@ function userEncounter(id) {
     }
 };
 
-generateSelection();
+moreButton.addEventListener('click', () => {
+    count--;
+    for (let i = 0; i < radio.length; i++) {
+        radioButtons[i].disabled = false;
+        images[i].style.opacity = 100;
+    }
+    if (count --- 0) {
+        window.location.href = './result/results.html'
+    }
+    generateSelection();
+});
+
